1a55d67ad21fa4ecd56233d38b98f78f38c61dbc
balkrishna <36560422+be-krishna@users.noreply.github.com>
2022-05-22 20:56:16 +0530

    Update index.js
    added required endpoints

--- index.js
+++ index.js
@@ -20,10 +20,35 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/", function(req, res) {
+  res.json({
+    unix: new Date().valueOf(),
+    utc: new Date().toUTCString()
+  });
 });
 
+
+app.get("/api/:date", function(req, res) {
+
+  let passedDate = req.params.date;
+  let dateString;
+
+  if (!isNaN(Number(passedDate))) {
+    dateString = new Date(Number(req.params.date));
+  } else {
+    dateString = new Date(req.params.date);
+  }
+  
+  if (dateString instanceof Date && !isNaN(dateString)) {
+    res.json({
+      unix: dateString.valueOf(),
+      utc: dateString.toUTCString()
+    });
+  } else {
+    res.json({ error: 'Invalid date' });
+  }
+
+});
 
 
 // listen for requests :)
